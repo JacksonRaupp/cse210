11bd0904be1b595e9057b70bd85573819e436fef
Jackson Raupp <JacksonRaupp@icloud.com>
2026-08-31 14:03:05 -0400

    Semana 01: Exercício 4: Listas e Genéricos

--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+        Console.WriteLine("Insira uma lista de números e digite 0 quando terminar.");
+
+        List<int> numeros = new List<int>();
+        int numero = -1;
+
+        while (numero != 0)
+        {
+            Console.Write("Insira o número: ");
+            numero = int.Parse(Console.ReadLine());
+            numeros.Add(numero);
+        }
+
+        int soma = 0;
+        float media;
+        int maiorNumero = 0;
+        int i;
+
+        for (i = 0; i < numeros.Count; i++)
+        {
+            soma += numeros[i];
+
+            if (numeros[i] > maiorNumero)
+            {
+                maiorNumero = numeros[i];
+            }
+        }
+
+        media = ((float)soma) / (i - 1);
+
+        Console.WriteLine($"A soma é: {soma}");
+        Console.WriteLine($"A média é: {media}");
+        Console.WriteLine($"A maior numero é: {maiorNumero}");
     }
 }
