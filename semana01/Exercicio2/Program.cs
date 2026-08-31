cc74f4c8a00965c40d3ed65e275b4e105f4f47fa
Jackson Raupp <JacksonRaupp@icloud.com>
2026-08-31 12:13:44 -0400

    Semana 01: Exercício 2: Declarações If

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Insira uma nota numérica: ");
+        string notaTexto = Console.ReadLine();
+
+        int notaNumero = int.Parse(notaTexto);
+
+        string notaLetra;
+
+        if (notaNumero >= 90)
+        {
+            notaLetra = "A";
+        }
+        else if (notaNumero >= 80)
+        {
+            notaLetra = "B";
+        }
+        else if (notaNumero >= 70)
+        {
+            notaLetra = "C";
+        }
+        else if (notaNumero >= 60)
+        {
+            notaLetra = "D";
+        }
+        else
+        {
+            notaLetra = "F";
+        }
+
+        Console.WriteLine($"Nota: {notaLetra}");
+
+        if (notaLetra == "A" || notaLetra == "B" || notaLetra == "C")
+        {
+            Console.WriteLine("Parabéns! Aprovado! 🥳");
+        }
+        else
+        {
+            Console.WriteLine("Não foi dessa vez! Mais sorte na próxima 🤞🏻");
+        }
+
     }
 }
