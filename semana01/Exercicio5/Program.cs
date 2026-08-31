5d8e57163e6c3f8b8d27671cdbfd9e65c680b2e7
Jackson Raupp <JacksonRaupp@icloud.com>
2026-08-31 14:41:28 -0400

    Semana 01: Exercício 5: Funções

--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        void ExibirBoasVindas()
+        {
+            Console.WriteLine("Bem-Vindo ao Programa!");
+        }
+
+        string PerguntarNomeUsuario()
+        {
+            Console.Write("Por favor, insira seu nome: ");
+            string nome = Console.ReadLine();
+            return nome;
+        }
+
+        int PerguntarNumeroFavorito()
+        {
+            Console.Write("Por favor, insira seu número favorito: ");
+            int numeroFavorito = int.Parse(Console.ReadLine());
+            return numeroFavorito;
+        }
+
+        int ElevarAoQuadrado(int numeroFavorito)
+        {
+            return numeroFavorito *= numeroFavorito;
+        }
+
+        void ExibirResultado(string nome, int quadrado)
+        {
+            Console.WriteLine($"Irmão {nome}, o quadrado do seu número é {quadrado}");
+        }
+
+        ExibirBoasVindas();
+        ExibirResultado(PerguntarNomeUsuario(), ElevarAoQuadrado(PerguntarNumeroFavorito()));
     }
 }
